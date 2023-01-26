a5cd051782c996f23747782a4630050275246f75
1peach <1337812310@qq.com>
2023-01-26 17:19:07 +0800

    outputMyProject

---
+++ MyProject/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MyProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hey");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bro\nCode");
+            Console.WriteLine("back\b");
+        }
+    }
+}
