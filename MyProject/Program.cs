c3eff4f8d8a2694a39f4b15c9030a60bc542dbdb
1peach <1337812310@qq.com>
2023-01-26 20:32:47 +0800

    variables

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -10,6 +10,32 @@
             Console.WriteLine("Bro\nCode");
             Console.WriteLine("back\ba");
             Console.Write("a\t\vb");
+
+            int x; //declaration
+            x = 123; //initialization
+
+            int y = 321;
+
+            int z = x + y;
+
+            int age = 21;//whole integer
+            double height = 300.5;//decimal number
+            bool alive = true;
+            char symbol= '@';
+            string name = "1peach";
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine("your age is " + age);
+            Console.WriteLine("your height is " + height + "cm");
+            Console.WriteLine("are you alive? " + alive);
+            Console.WriteLine("your symbol is " + symbol);
+
+            string userName = symbol + name;
+            Console.WriteLine("Your username is: " + userName);
+
             Console.ReadKey();
         }
     }
